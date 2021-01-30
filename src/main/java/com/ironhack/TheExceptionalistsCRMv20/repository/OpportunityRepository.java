9ca9fe2dbcd03433d3258c30e7744f96d161c140
adrialopezbou <adrialopezbou@hotmail.com>
2021-01-30 21:03:25 +0100

    Added some JPQL queries and Tests for Opportunity

--- src/main/java/com/ironhack/TheExceptionalistsCRMv20/repository/OpportunityRepository.java
+++ src/main/java/com/ironhack/TheExceptionalistsCRMv20/repository/OpportunityRepository.java
@@ -2,8 +2,58 @@ package com.ironhack.TheExceptionalistsCRMv20.repository;
 
 import com.ironhack.TheExceptionalistsCRMv20.model.Opportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, String> {
+
+    //Returns a list of names and count of all Opportunities by SalesRep
+    @Query(value = "SELECT s.name, COUNT(*) FROM SalesRep s JOIN FETCH Opportunity o ON s.id = o.salesRep " +
+            "GROUP BY s.name")
+    List<Object[]> countOfOpportunitiesBySalesReps();
+
+    //Returns a list of names and count of all Opportunities by SalesRep where status is CLOSED_WON
+    @Query(value = "SELECT s.name, COUNT(*) FROM SalesRep s JOIN FETCH Opportunity o ON s.id = o.salesRep " +
+            "WHERE o.status = com.ironhack.TheExceptionalistsCRMv20.enums.Status.CLOSED_WON GROUP BY s.name")
+    List<Object[]> countOfOpportunitiesBySalesRepsWhereClosedWon();
+
+    //Returns a list of names and count of all Opportunities by SalesRep where status is CLOSED_LOST
+    @Query(value = "SELECT s.name, COUNT(*) FROM SalesRep s JOIN FETCH Opportunity o ON s.id = o.salesRep " +
+            "WHERE o.status = com.ironhack.TheExceptionalistsCRMv20.enums.Status.CLOSED_LOST GROUP BY s.name")
+    List<Object[]> countOfOpportunitiesBySalesRepsWhereClosedLost();
+
+    //Returns a list of names and count of all Opportunities by SalesRep where status is OPEN
+    @Query(value = "SELECT s.name, COUNT(*) FROM SalesRep s JOIN FETCH Opportunity o ON s.id = o.salesRep " +
+            "WHERE o.status = com.ironhack.TheExceptionalistsCRMv20.enums.Status.OPEN GROUP BY s.name")
+    List<Object[]> countOfOpportunitiesBySalesRepsWhereOpen();
+
+    //Returns a list of products and count of all Opportunities by Product
+    @Query(value = "SELECT product, COUNT(*) FROM Opportunity GROUP BY product")
+    List<Object[]> countOfOpportunitiesByProduct();
+
+    //Returns a list of products and count of all Opportunities by Product where status is CLOSED_WON
+    @Query(value = "SELECT product, COUNT(*) FROM Opportunity " +
+            "WHERE status = com.ironhack.TheExceptionalistsCRMv20.enums.Status.CLOSED_WON GROUP BY product")
+    List<Object[]> countOfOpportunitiesByProductWhereClosedWon();
+
+    //Returns a list of products and count of all Opportunities by Product where status is CLOSED_LOST
+    @Query(value = "SELECT product, COUNT(*) FROM Opportunity " +
+            "WHERE status = com.ironhack.TheExceptionalistsCRMv20.enums.Status.CLOSED_LOST GROUP BY product")
+    List<Object[]> countOfOpportunitiesByProductWhereClosedLost();
+
+    //Returns a list of products and count of all Opportunities by Product where status is OPEN
+    @Query(value = "SELECT product, COUNT(*) FROM Opportunity " +
+            "WHERE status = com.ironhack.TheExceptionalistsCRMv20.enums.Status.OPEN GROUP BY product")
+    List<Object[]> countOfOpportunitiesByProductWhereOpen();
+
+
+
+    //Returns a count of Opportunities for a specific SalesRep Id
+    Integer countBySalesRepId(String salesRepId);
+    //Return a count of Opportunities for a specific SalesRep Name
+    Integer countBySalesRepName(String salesRep);
 }
---
+++ src/test/java/com/ironhack/TheExceptionalistsCRMv20/repository/OpportunityRepositoryTest.java
@@ -0,0 +1,174 @@
+package com.ironhack.TheExceptionalistsCRMv20.repository;
+
+import com.ironhack.TheExceptionalistsCRMv20.enums.Industry;
+import com.ironhack.TheExceptionalistsCRMv20.enums.Product;
+import com.ironhack.TheExceptionalistsCRMv20.enums.Status;
+import com.ironhack.TheExceptionalistsCRMv20.model.Account;
+import com.ironhack.TheExceptionalistsCRMv20.model.Contact;
+import com.ironhack.TheExceptionalistsCRMv20.model.Opportunity;
+import com.ironhack.TheExceptionalistsCRMv20.model.SalesRep;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class OpportunityRepositoryTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    OpportunityRepository opportunityRepository;
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    SalesRepRepository salesRepRepository;
+
+    @BeforeEach
+    void setUp() {
+        Contact contact1 = contactRepository.save(new Contact("co001", "Pedro Luis",
+                "pedro.luis@gmail.com", "IKEA", "666 333 222 111"));
+        Contact contact2 = contactRepository.save(new Contact("co002", "Pedro Luis",
+                "pedro.luis@gmail.com", "IKEA", "666 333 222 111"));
+        Contact contact3 = contactRepository.save(new Contact("co003", "Pedro Luis",
+                "pedro.luis@gmail.com", "IKEA", "666 333 222 111"));
+        Contact contact4 = contactRepository.save(new Contact("co004", "Pedro Luis",
+                "pedro.luis@gmail.com", "IKEA", "666 333 222 111"));
+        SalesRep salesRep = salesRepRepository.save(new SalesRep("sr005", "María Aguilar"));
+        Opportunity opportunity1 = opportunityRepository.save(new Opportunity("op006", Product.HYBRID, 10, contact1,
+                Status.OPEN));
+        Opportunity opportunity2 = opportunityRepository.save(new Opportunity("op007", Product.BOX, 10, contact2,
+                Status.CLOSED_WON));
+        Opportunity opportunity3 = opportunityRepository.save(new Opportunity("op008", Product.HYBRID, 10, contact3,
+                Status.CLOSED_WON));
+        Opportunity opportunity4 = opportunityRepository.save(new Opportunity("op009", Product.FLATBED, 10, contact4,
+                Status.CLOSED_LOST));
+        Account account = accountRepository.save(new Account("ac010", "IKEA", Industry.ECOMMERCE,
+                200, "Madrid", "Spain", contact1, opportunity1));
+        opportunity1.setAccount(account);
+        opportunity1.setSalesRep(salesRep);
+        opportunity1 = opportunityRepository.save(opportunity1);
+        opportunity2.setAccount(account);
+        opportunity2.setSalesRep(salesRep);
+        opportunity2 = opportunityRepository.save(opportunity2);
+        opportunity3.setAccount(account);
+        opportunity3.setSalesRep(salesRep);
+        opportunity3 = opportunityRepository.save(opportunity3);
+        opportunity4.setAccount(account);
+        opportunity4.setSalesRep(salesRep);
+        opportunity4 = opportunityRepository.save(opportunity4);
+        contact1.setAccount(account);
+        contact1 = contactRepository.save(contact1);
+        account.setContactList(List.of(contact1));
+        account.setOpportunityList(List.of(opportunity1));
+        contact2.setAccount(account);
+        contact2 = contactRepository.save(contact2);
+        account.setContactList(List.of(contact2));
+        account.setOpportunityList(List.of(opportunity2));
+        contact3.setAccount(account);
+        contact3 = contactRepository.save(contact3);
+        account.setContactList(List.of(contact3));
+        account.setOpportunityList(List.of(opportunity3));
+        contact4.setAccount(account);
+        contact4 = contactRepository.save(contact4);
+        account.setContactList(List.of(contact4));
+        account.setOpportunityList(List.of(opportunity4));
+        account = accountRepository.save(account);
+    }
+
+    @AfterEach
+    void tearDown() {
+        contactRepository.deleteAll();
+        opportunityRepository.deleteAll();
+        salesRepRepository.deleteAll();
+        accountRepository.deleteAll();
+    }
+
+    @Test
+    void countBySalesRep_salesRepExistingId_count() {
+        assertEquals(4, opportunityRepository.countBySalesRepId("sr005"));
+    }
+
+    @Test
+    void countBySalesRepName_salesRepExistingName_count() {
+        assertEquals(4, opportunityRepository.countBySalesRepName("María Aguilar"));
+    }
+
+    @Test
+    void countOfOpportunitiesBySalesReps_salesRepExistent_listOfNameAndCount() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesBySalesReps();
+        assertEquals(1, result.size());
+        assertEquals("María Aguilar", result.get(0)[0]);
+        assertEquals(4L, result.get(0)[1]);
+    }
+
+
+    @Test
+    void countOfOpportunitiesBySalesRepsWhereClosedWon_salesRepExistent_listOfNameAndCount() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesBySalesRepsWhereClosedWon();
+        assertEquals(1, result.size());
+        assertEquals("María Aguilar", result.get(0)[0]);
+        assertEquals(2L, result.get(0)[1]);
+    }
+
+    @Test
+    void countOfOpportunitiesBySalesRepsWhereClosedLost_salesRepExistent_listOfNameAndCount() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesBySalesRepsWhereClosedLost();
+        assertEquals(1, result.size());
+        assertEquals("María Aguilar", result.get(0)[0]);
+        assertEquals(1L, result.get(0)[1]);
+    }
+
+    @Test
+    void countOfOpportunitiesBySalesRepsWhereOpen_salesRepExistent_listOfNameAndCount() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesBySalesRepsWhereOpen();
+        assertEquals(1, result.size());
+        assertEquals("María Aguilar", result.get(0)[0]);
+        assertEquals(1L, result.get(0)[1]);
+    }
+
+    @Test
+    void countOfOpportunitiesByProduct_containsOpportunities_listOfProductAndCount() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesByProduct();
+        assertEquals(3L, result.size());
+        assertEquals(Product.HYBRID, result.get(0)[0]);
+        assertEquals(2L, result.get(0)[1]);
+        assertEquals(Product.BOX, result.get(1)[0]);
+        assertEquals(1L, result.get(1)[1]);
+        assertEquals(Product.FLATBED, result.get(2)[0]);
+        assertEquals(1L, result.get(2)[1]);
+    }
+
+    @Test
+    void countOfOpportunitiesByProductWhereClosedWon() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesByProductWhereClosedWon();
+        assertEquals(2L, result.size());
+        assertEquals(Product.BOX, result.get(0)[0]);
+        assertEquals(1L, result.get(0)[1]);
+        assertEquals(Product.HYBRID, result.get(1)[0]);
+        assertEquals(1L, result.get(1)[1]);
+    }
+
+    @Test
+    void countOfOpportunitiesByProductWhereClosedLost() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesByProductWhereClosedLost();
+        assertEquals(1L, result.size());
+        assertEquals(Product.FLATBED, result.get(0)[0]);
+        assertEquals(1L, result.get(0)[1]);
+    }
+
+    @Test
+    void countOfOpportunitiesByProductWhereOpen() {
+        List<Object[]> result = opportunityRepository.countOfOpportunitiesByProductWhereOpen();
+        assertEquals(1L, result.size());
+        assertEquals(Product.HYBRID, result.get(0)[0]);
+        assertEquals(1L, result.get(0)[1]);
+    }
+}
