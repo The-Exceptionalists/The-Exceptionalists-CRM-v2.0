bd5e0966a5efc0594a02981512f51d14efa17904
scorsaro <salvatore.corsaro@live.com>
2021-01-30 10:28:08 +0100

    first coomand line app

---
+++ src/main/java/com/ironhack/TheExceptionalistsCRMv20/ConsoleApp.java
@@ -0,0 +1,38 @@
+package com.ironhack.TheExceptionalistsCRMv20;
+
+
+import com.ironhack.TheExceptionalistsCRMv20.manager.CommandManager;
+import com.ironhack.TheExceptionalistsCRMv20.utilities.Buffer;
+import com.ironhack.TheExceptionalistsCRMv20.utilities.Output;
+import com.ironhack.TheExceptionalistsCRMv20.utilities.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Scanner;
+
+
+@SpringBootApplication
+public class ConsoleApp implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleApp.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(ConsoleApp.class, args);
+        Output.introResolutionAlert();
+        Buffer.initStringsRepository();
+        State.restoreState();
+        while (true) {
+            CommandManager.introduceCommand();
+        }
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("welcome to my app");
+    }
+}
