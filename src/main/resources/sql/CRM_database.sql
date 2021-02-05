f3097e63221f0d2b30d3a0de4bd7b35050049bab
Jaume <jasato89@gmail.com>
2021-02-05 14:04:06 +0100

    Fixed .sql file

--- src/main/resources/sql/CRM_database.sql
+++ src/main/resources/sql/CRM_database.sql
@@ -1,77 +1,69 @@
-DROP SCHEMA IF EXISTS crm;
+CREATE SCHEMA crm_exceptionalists;
+USE crm_exceptionalists;
+CREATE TABLE IF NOT EXISTS `accounts` (
+                                          `id` INT NOT NULL AUTO_INCREMENT,
+                                          `city` VARCHAR(255) NULL DEFAULT NULL,
+                                          `company_name` VARCHAR(255) NULL DEFAULT NULL,
+                                          `country` VARCHAR(255) NULL DEFAULT NULL,
+                                          `employee_count` INT NOT NULL,
+                                          `industry` VARCHAR(255) NULL DEFAULT NULL,
+                                          PRIMARY KEY (`id`));
 
-CREATE SCHEMA crm;
-USE crm;
+CREATE TABLE IF NOT EXISTS `contacts` (
+                                          `id` INT NOT NULL,
+                                          `account_id` INT NULL DEFAULT NULL,
+                                          PRIMARY KEY (`id`),
+                                          INDEX `FKl4tvq8qk7x11e16v2cnp1mja8` (`account_id` ASC) VISIBLE,
+                                          CONSTRAINT `FK919o4dqu6vc2amoqtvifp1pp7`
+                                              FOREIGN KEY (`id`)
+                                                  REFERENCES `crm-latest`.`items` (`id`),
+                                          CONSTRAINT `FKl4tvq8qk7x11e16v2cnp1mja8`
+                                              FOREIGN KEY (`account_id`)
+                                                  REFERENCES `crm-latest`.`accounts` (`id`));
 
-CREATE TABLE sales_rep
-(
-    id   VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS `items` (
+                                       `id` INT NOT NULL AUTO_INCREMENT,
+                                       `company_name` VARCHAR(255) NULL DEFAULT NULL,
+                                       `email` VARCHAR(255) NULL DEFAULT NULL,
+                                       `name` VARCHAR(255) NULL DEFAULT NULL,
+                                       `phone_number` VARCHAR(255) NULL DEFAULT NULL,
+                                       PRIMARY KEY (`id`));
 
-CREATE TABLE items
-(
-    id           VARCHAR(255) NOT NULL,
-    name         VARCHAR(255),
-    phone_number VARCHAR(255),
-    email        VARCHAR(255),
-    company_name VARCHAR(255),
-    PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS `leads` (
+                                       `id` INT NOT NULL,
+                                       `sales_rep_id` INT NULL DEFAULT NULL,
+                                       PRIMARY KEY (`id`),
+                                       INDEX `FKfxo14wqvkc8ba27nyy13owb14` (`sales_rep_id` ASC) VISIBLE,
+                                       CONSTRAINT `FKfxo14wqvkc8ba27nyy13owb14`
+                                           FOREIGN KEY (`sales_rep_id`)
+                                               REFERENCES `crm-latest`.`sales_rep` (`id`),
+                                       CONSTRAINT `FKmplheine5gemdfrxr1o2bm4en`
+                                           FOREIGN KEY (`id`)
+                                               REFERENCES `crm-latest`.`items` (`id`));
 
-CREATE TABLE leads
-(
-    id           VARCHAR(255) NOT NULL,
-    sales_rep_id VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (id)
-        REFERENCES items (id),
-    FOREIGN KEY (sales_rep_id)
-        REFERENCES sales_rep (id)
-);
+CREATE TABLE IF NOT EXISTS `opportunities` (
+                                               `id` INT NOT NULL AUTO_INCREMENT,
+                                               `product` VARCHAR(255) NULL DEFAULT NULL,
+                                               `quantity` INT NOT NULL,
+                                               `status` VARCHAR(255) NULL DEFAULT NULL,
+                                               `account_id` INT NULL DEFAULT NULL,
+                                               `decision_maker_id` INT NULL DEFAULT NULL,
+                                               `sales_rep_id` INT NULL DEFAULT NULL,
+                                               PRIMARY KEY (`id`),
+                                               INDEX `FKeyutus9ng4fxunsxib6elbxrt` (`account_id` ASC) VISIBLE,
+                                               INDEX `FKm2r9f7fkemlc1ku8vxqan6hll` (`decision_maker_id` ASC) VISIBLE,
+                                               INDEX `FKajd23elxn1a79ydvpks3noab0` (`sales_rep_id` ASC) VISIBLE,
+                                               CONSTRAINT `FKajd23elxn1a79ydvpks3noab0`
+                                                   FOREIGN KEY (`sales_rep_id`)
+                                                       REFERENCES `crm-latest`.`sales_rep` (`id`),
+                                               CONSTRAINT `FKeyutus9ng4fxunsxib6elbxrt`
+                                                   FOREIGN KEY (`account_id`)
+                                                       REFERENCES `crm-latest`.`accounts` (`id`),
+                                               CONSTRAINT `FKm2r9f7fkemlc1ku8vxqan6hll`
+                                                   FOREIGN KEY (`decision_maker_id`)
+                                                       REFERENCES `crm-latest`.`contacts` (`id`));
 
-CREATE TABLE contacts
-(
-    id         VARCHAR(255) NOT NULL,
-    account_id VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (id)
-        REFERENCES items (id),
-    FOREIGN KEY (account_id)
-        REFERENCES accounts (id)
-);
-
-
-CREATE TABLE accounts
-(
-    id             VARCHAR(255) NOT NULL,
-    company_name   VARCHAR(255),
-    industry       VARCHAR(255),
-    employee_count INT,
-    city           VARCHAR(255),
-    country        VARCHAR(255),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE opportunities
-(
-    id                VARCHAR(255) NOT NULL,
-    quantity          INT,
-    decision_maker_id VARCHAR(255),
-    status            VARCHAR(255),
-    product           VARCHAR(255),
-    account_id        VARCHAR(255),
-    sales_rep_id      VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (decision_maker)
-        REFERENCES contacts (id),
-    FOREIGN KEY (account_id)
-        REFERENCES accounts (id),
-    FOREIGN KEY (sales_rep_id)
-        REFERENCES sales_rep (id)
-);
-    
-    
-    
-    
+CREATE TABLE IF NOT EXISTS `sales_rep` (
+                                           `id` INT NOT NULL AUTO_INCREMENT,
+                                           `name` VARCHAR(255) NULL DEFAULT NULL,
+                                           PRIMARY KEY (`id`));
